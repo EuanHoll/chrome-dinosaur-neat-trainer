774a65a648e542919711096938eb4aeca6c65fc9
EHoll <ehollidge@hotmail.com>
2022-04-30 12:21:20 +0200

    Able to Run network without training

--- src/main.py
+++ src/main.py
@@ -4,6 +4,8 @@ import random
 import sys
 import neat
 import math
+import pickle
+import argparse
 
 pygame.init()
 
@@ -15,6 +17,8 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 # Location Variables
 home_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 config_path = os.path.join(home_folder, "rsrc/config.txt")
+winner_path = os.path.join(home_folder, "rsrc/winner.nn")
+input_path = os.path.join(home_folder, "rsrc/winner.nn")
 
 # Static Game Variables
 dino = {
@@ -47,10 +51,17 @@ score = 0
 score_text = "Score: " + str(score)
 obstacles = []
 dinosaurs = []
+obstacle_spawned = False
 
 # Network Variables
 genomes_list = []
 nueral_nets = []
+pop = None
+use_previous_winner = True
+
+# Program Variables
+mode = "train"
+generations = 10
 
 
 class Dinosaur(object):
@@ -123,6 +134,10 @@ class Obstacle:
         screen.blit(self.images[self.type], self.rect)
 
 
+class EmptyPop(object):
+    generation = 0
+
+
 class SmallCactus(Obstacle):
     def __init__(self, number_of_obstacles=1):
         global small_cactus
@@ -169,7 +184,7 @@ def manage_background():
 
 
 def spawn_obstacles():
-    global obstacles
+    global obstacles, obstacle_spawned
 
     if len(obstacles) == 0:
         rand_int = random.randint(0, 1)
@@ -177,6 +192,7 @@ def spawn_obstacles():
             obstacles.append(SmallCactus(random.randint(1, 3)))
         elif rand_int == 1:
             obstacles.append(LargeCactus(random.randint(1, 3)))
+        obstacle_spawned = True
 
 
 def remove_dino(index):
@@ -193,19 +209,32 @@ def manage_obstacles():
     for obstacle in obstacles:
         obstacle.draw(screen)
         obstacle.update()
-        feed_forward(obstacle)
         for i, dinosaur in enumerate(dinosaurs):
             if dinosaur.rect.colliderect(obstacle.rect):
-                genomes_list[i].fitness += score
+                genomes_list[i].fitness = score
                 remove_dino(i)
 
 
-def setup_dinosaurs(genomes, config):
-    global dinosaurs, genomes_list, nueral_nets
+def training_setup(genomes, config):
+    global dinosaurs, genomes_list, nueral_nets, game_speed, background_x_pos, background_y_pos, score, obstacle_spawned, obstacles, use_previous_winner, winner_path
 
     dinosaurs = []
     genomes_list = []
     nueral_nets = []
+    obstacles = []
+    game_speed = 15
+    background_x_pos = 0
+    background_y_pos = 380
+    score = 0
+    obstacle_spawned = False
+
+    if use_previous_winner and os.path.exists(winner_path):
+        with open(winner_path, "rb") as f:
+            genome_winner = pickle.load(f)
+        genomes_list.append(genome_winner)
+        net = neat.nn.FeedForwardNetwork.create(genome_winner, config)
+        nueral_nets.append(net)
+        genome_winner.fitness = 0
 
     for genome_id, genome in genomes:
         dinosaurs.append(Dinosaur())
@@ -221,19 +250,38 @@ def distance(pos_a, pos_b):
     return math.sqrt(dx**2 + dy**2)
 
 
-def feed_forward(obstacle):
-    global dinosaurs, genomes_list, nueral_nets
+def feed_forward():
+    global dinosaurs, genomes_list, nueral_nets, obstacle_spawned, obstacles
 
     for i, dinosaur in enumerate(dinosaurs):
         output = nueral_nets[i].activate(
             (
                 dinosaur.rect.y,
-                distance((dinosaur.rect.x, dinosaur.rect.y), obstacle.rect.midtop),
+                distance((dinosaur.rect.x, dinosaur.rect.y), obstacles[0].rect.midtop)
+                if len(obstacles) >= 1
+                else 500,
             )
         )
         if output[0] > 0.5 and dinosaur.rect.y == dinosaur.default_y_pos:
             dinosaur.dino_jump = True
             dinosaur.dino_run = False
+        obstacle_spawned = False
+
+
+def statistics():
+    global dinosaurs, genomes_list, game_speed, pop, font, screen, generations
+
+    num_of_dinos_alive = font.render(
+        "Dinosaurs Alive: " + str(len(dinosaurs)), True, (0, 0, 0)
+    )
+    generation_number = font.render(
+        "Generation: " + str(pop.generation) + " / " + str(generations), True, (0, 0, 0)
+    )
+    game_speed_surface = font.render("Game Speed: " + str(game_speed), True, (0, 0, 0))
+
+    screen.blit(num_of_dinos_alive, (50, 450))
+    screen.blit(generation_number, (50, 480))
+    screen.blit(game_speed_surface, (50, 510))
 
 
 def eval(genomes, config):
@@ -241,7 +289,7 @@ def eval(genomes, config):
 
     clock = pygame.time.Clock()
 
-    setup_dinosaurs(genomes, config)
+    training_setup(genomes, config)
 
     run = True
     while run:
@@ -265,15 +313,123 @@ def eval(genomes, config):
 
         spawn_obstacles()
         manage_obstacles()
+        feed_forward()
 
         manage_background()
         increase_score()
         render_score()
+        statistics()
         clock.tick(30)
         pygame.display.update()
 
 
-def run(config_path):
+def train():
+    global pop, winner_path, config_path, generations
+
+    config = neat.config.Config(
+        neat.DefaultGenome,
+        neat.DefaultReproduction,
+        neat.DefaultSpeciesSet,
+        neat.DefaultStagnation,
+        config_path,
+    )
+    pop = neat.Population(config)
+    winner = pop.run(eval, generations)
+    with open(winner_path, "wb") as f:
+        pickle.dump(winner, f)
+
+
+def str_to_bool(v):
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ("yes", "true", "t", "y", "1"):
+        return True
+    elif v.lower() in ("no", "false", "f", "n", "0"):
+        return False
+    raise argparse.ArgumentTypeError("Boolean value expected.")
+
+
+def train_or_run(v):
+    if v == "train" or v == "run":
+        return v
+    raise argparse.ArgumentTypeError("'train' or 'run' expected.")
+
+
+def valid_path(v):
+    if v != "":
+        if os.path.exists(v) and os.path.isfile(v):
+            return v
+        elif os.path.exists(v) and not os.path.isfile(v):
+            raise argparse.ArgumentTypeError("Path must be an existing file.")
+    return v
+
+
+def existing_path(v):
+    if v != "":
+        if os.path.exists(v) and os.path.isfile(v):
+            return v
+        raise argparse.ArgumentTypeError("Path must be an existing file.")
+    return v
+
+
+def positive_int(v):
+    v = int(v)
+    if v > 0:
+        return v
+    raise argparse.ArgumentTypeError("Value must be greater than 0.")
+
+
+def parse_args():
+    global winner_path, use_previous_winner, mode, input_path, generations
+
+    parser = argparse.ArgumentParser(
+        description="Run a Neat Algorithm to Play / Train on the Chrome Dinosaur Game"
+    )
+    parser.add_argument(
+        "-wp",
+        "--winner_path",
+        type=valid_path,
+        default="",
+        help="Path to save the winner file to.",
+    )
+    parser.add_argument(
+        "-ip", "--input_path", type=existing_path, default="", help="Path to"
+    )
+    parser.add_argument(
+        "-up",
+        "--use_previous",
+        type=str_to_bool,
+        default="False",
+        help="Use previous winner in the first generation of training.",
+    )
+    parser.add_argument(
+        "-m",
+        "--mode",
+        type=train_or_run,
+        default="train",
+        help="Whether the program is set to 'train' or 'run' mode.",
+    )
+    parser.add_argument(
+        "-g",
+        "--generations",
+        type=positive_int,
+        default=10,
+        help="Number of generations the networks will train for. Must be greater than 0.",
+    )
+
+    args = parser.parse_args()
+    if args.winner_path != "":
+        winner_path = args.winner_path
+    if args.input_path != "":
+        input_path = args.input_path
+    use_previous_winner = args.use_previous
+    mode = args.mode
+    generations = args.generations
+
+
+def run():
+    global config_path, input_path, pop, generations
+
     config = neat.config.Config(
         neat.DefaultGenome,
         neat.DefaultReproduction,
@@ -282,9 +438,18 @@ def run(config_path):
         config_path,
     )
 
-    pop = neat.Population(config)
-    pop.run(eval, 50)
+    with open(input_path, "rb") as f:
+        genome_winner = pickle.load(f)
+
+    gnomes = [(1, genome_winner)]
+    pop = EmptyPop()
+    generations = 0
+    eval(gnomes, config)
 
 
 if __name__ == "__main__":
-    run(config_path)
+    parse_args()
+    if mode == "train":
+        train()
+    elif mode == "run":
+        run()
